1d8855bd5a73d506f4fd4f7bc6c82587070231f9
Evgenij <murzin90@yandex.ru>
2022-08-03 08:33:44 +0700

    Add actions 2

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,14 @@ $('button').on('click', function() {
     $('div').eq(2).toggleClass('active');
 });
 
-console.log($('button').html('hello'));
+// console.log($('button').html('hello'));
 
 $('div').click(function() {
     console.log($(this).index());
 });
 
-console.log($('div').eq(2).find('.more'));
+// console.log($('div').eq(2).find('.more'));
+
+// console.log($('.some').closest('.findme'));
+
+console.log($('.more').eq(0).siblings());
